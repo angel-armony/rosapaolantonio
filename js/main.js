4b547b8862ff489c595e870edbaa411875f91a94
angel-armony <paolantonioannarosa@gmail.com>
2025-08-18 20:54:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -159,18 +159,18 @@ window.addEventListener('load', function() {
     const img = document.querySelector('img[src*="imgHome.png"]');
     
     if (img && window.innerWidth >= 1025) {
-        img.src = img.src.replace('imgHome.png', 'imgHomeDesktop.png');
+        img.src = img.src.replace('imgHome.png', 'ImgHomeDesktop.png');
     }
 });
 
 window.addEventListener('resize', function() {
-    const img = document.querySelector('img[src*="imgHome"], img[src*="imgHomeDesktop"]');
+    const img = document.querySelector('img[src*="imgHome"], img[src*="ImgHomeDesktop"]');
     
     if (img) {
         if (window.innerWidth >= 1025) {
-            img.src = img.src.replace('imgHome.png', 'imgHomeDesktop.png');
+            img.src = img.src.replace('imgHome.png', 'ImgHomeDesktop.png');
         } else {
-            img.src = img.src.replace('imgHomeDesktop.png', 'imgHome.png');
+            img.src = img.src.replace('imgHomeDesktop.png', 'ImgHome.png');
         }
     }
 });
